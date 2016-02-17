31f2f9987bab9ccc2b8c20333caedfb019ea58e6
Rui Serra <ruipserra@gmail.com>
2016-02-17 08:31:03 +0000

    Chapter 5: Switching views

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,8 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use sdl2::pixels::Color;
-use phi::Events;
-
 fn main() {
-    // Initizalize SDL2
-    let sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-
-    // Create the window
-    let window = video.window("ArcadeRS Shooter", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let mut renderer = window.renderer()
-        .accelerated()
-        .build()
-        .unwrap();
-
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
-
-    loop {
-        events.pump();
-
-        if events.now.key_escape == Some(true) || events.now.quit {
-            break;
-        }
-
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
-        renderer.present();
-    }
+    ::phi::spawn("ArcadeRS Shooter", |_| {
+        Box::new(::views::ViewA)
+    });
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -2,12 +2,14 @@
 mod events;
 
 use sdl2::render::Renderer;
+use sdl2::pixels::Color;
 
 struct_events! {
     keyboard: {
         key_escape: Escape,
         key_up: Up,
-        key_down: Down
+        key_down: Down,
+        key_space: Space
     },
 
     else: {
@@ -25,6 +27,7 @@ pub struct Phi<'window> {
 pub enum ViewAction {
     None,
     Quit,
+    ChangeView(Box<View>),
 }
 
 pub trait View {
@@ -32,4 +35,87 @@ pub trait View {
     /// with the render loop. Called on every frame.
     // TODO: separate update and render logic?
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
+
+/// Create a window with name `title`, initialize the underlying libraries and
+/// start the game with the view returned by `init()`.
+///
+/// # Examples
+///
+/// Here, we simply show a window with color #ffff00 and exit when escape is
+/// pressed or when the window is closed.
+///
+/// ```
+/// struct MyView;
+/// impl View for MyView {
+///     fn render(&mut self, context: &mut Phi, _: f64) -> ViewAction {
+///         if context.events.now.quit {
+///             return ViewAction::Quit;
+///         }
+///
+///         context.renderer.set_draw_color(Color::RGB(255, 255, 0));
+///         context.renderer.clear();
+///         ViewAction::None
+///     }
+/// }
+///
+/// spawn("Example", |_| Box::new(MyView));
+/// ```
+pub fn spawn<F>(title: &str, init: F)
+    where F: Fn(&mut Phi) -> Box<View> {
+
+    // Initizalize SDL2
+    let sdl_context = ::sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window("ArcadeRS Shooter", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.renderer()
+            .accelerated()
+            .build().unwrap(),
+    };
+
+    let mut current_view = init(&mut context);
+
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+    loop {
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+        context.events.pump();
+
+        match current_view.render(&mut context, 0.01) {
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break,
+            ViewAction::ChangeView(new_view) => current_view = new_view,
+        }
+    }
 }
